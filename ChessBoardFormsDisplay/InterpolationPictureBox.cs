31be4602e013c3e3a544e88a3019967c329b319c
sealed interface <sealedinterface@gmail.com>
2018-09-06 11:47:29 -0700

    Added basic display projects

---
+++ ChessBoardFormsDisplay/InterpolationPictureBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessBoardFormsDisplay
+{
+	public class InterpolationPictureBox : PictureBox
+	{
+		public InterpolationMode Interpolation
+		{ get; set; }
+
+		protected override void OnPaint(PaintEventArgs pe)
+		{
+			pe.Graphics.InterpolationMode = Interpolation;
+			base.OnPaint(pe);
+		}
+	}
+}
